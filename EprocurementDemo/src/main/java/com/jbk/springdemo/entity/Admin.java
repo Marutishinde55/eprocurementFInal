747f7dc40adb46559e6430ee563eb8e2622fa755
sachin bhavar <bhavarsachin@gmail.com>
2020-04-01 16:09:32 +0530

    dads

--- EprocurementDemo/src/main/java/com/jbk/springdemo/entity/Admin.java
+++ EprocurementDemo/src/main/java/com/jbk/springdemo/entity/Admin.java
@@ -16,7 +16,7 @@ public class Admin {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
-	
+	//helllo
 	@NotNull()
 	@Size(min=1,message = "enter valid username")
 	@Column(name = "username")
